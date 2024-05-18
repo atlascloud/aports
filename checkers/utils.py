b6d4d7815815036de2d8dbc096e4b4668f28316d
iggy <iggy@atlascloud.xyz>
2024-05-18 15:25:13 -0700

    update checkers to better ignore certain strings

--- checkers/utils.py
+++ checkers/utils.py
@@ -2,51 +2,65 @@ import requests
 # from pkg_resources import parse_version # deprecated
 from packaging import version
 
-def get_latest_gh_tag(proj: str, filter_not: str = None) -> str:
+def get_latest_gh_tag(proj: str, must_have: str = None, ignore: str = None) -> str:
     """find latest version based on gh tags
 
     :param proj:
         github project i.e. ``ceph/ceph``
     """
-    highest_version = '0.0.0'
+    highest_version = version.parse('0.0.0')
 
-    r = requests.get('https://api.github.com/repos/' + proj + '/tags')
+    r = requests.get('https://api.github.com/repos/' + proj + '/tags?per_page=100')
 
     j = r.json()
 
     for t in j:
-        if t['name'].startswith('v'):
-            vers = t['name'].replace('v', '')
-            # # in ceph land X.2.X versions are the current stable
-            if filter_not:
-                if filter_not not in vers:
+        if t.get('name') and t['name'] != "":
+            try:
+                vers = version.parse(t['name'])
+            except:
+                # probably some weird tag (I've seen nightly as an example)
+                continue
+            if must_have:
+                if must_have not in t['name']:
                     continue
-            if version.parse(vers) > version.parse(highest_version):
+            if ignore:
+                if ignore in t['name']:
+                    continue
+            if vers > highest_version:
                 highest_version = vers
 
     return highest_version
 
 
-def get_latest_gh_releases(proj: str, filter_not: str = None) -> str:
-    """find latest version based on gh tags
+def get_latest_gh_releases(proj: str, must_have: str = None, ignore: str = None) -> str:
+    """find latest version based on gh releases
 
     :param proj:
-        github project i.e. ``ceph/ceph``
+        github project i.e. ``adguardteam/adguardhome``
     """
-    highest_version = '0.0.0'
+    highest_version = version.parse('0.0.0')
 
-    r = requests.get('https://api.github.com/repos/' + proj + '/tags')
+    r = requests.get('https://api.github.com/repos/' + proj + '/releases?per_page=100')
 
     j = r.json()
 
     for t in j:
-        if t['name'].startswith('v'):
-            vers = t['name'].replace('v', '')
-            # # in ceph land X.2.X versions are the current stable
-            if filter_not:
-                if filter_not not in vers:
+        # I think tag_name is safer to use here than name, but they seem to be
+        # the same in most of the stuff I looked at
+        if t.get('tag_name') and t['tag_name'] != "":
+            try:
+                vers = version.parse(t['tag_name'])
+            except:
+                # probably some weird tag (I've seen nightly as an example)
+                continue
+            if must_have:
+                if must_have not in t['tag_name']:
                     continue
-            if version.parse(vers) > version.parse(highest_version):
+            if ignore:
+                if ignore in t['tag_name']:
+                    continue
+            if vers > highest_version:
                 highest_version = vers
 
     return highest_version
